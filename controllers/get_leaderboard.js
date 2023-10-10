e04c97d11b81d6f13e011062b016c80e013969b4
gbhat <ganesh.bhat1@sony.com>
2023-10-10 14:51:21 +0530

    Implement getLeaderboard API

---
+++ controllers/get_leaderboard.js
@@ -0,0 +1,16 @@
+const User = require("../models/user");
+const asyncHandler = require("express-async-handler");
+
+
+const getLeaderboard = asyncHandler(async (req, res) => {
+    try {
+      const result = await User.find().sort({ "score" : "desc"})
+      console.log(result);
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500);
+      throw new Error(err.message);
+    }
+  });
+  
+module.exports = getLeaderboard;
--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const { default: mongoose } = require("mongoose");
 const connectToMongo = require("./utils/connection");
 const register = require("./routes/register_user");
 const updateScore = require('./routes/update_score');
+const getLeaderboard = require('./routes/get_leaderboard');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/api/v1", getLeaderboard)
 app.use("/api/v1", register);
 app.use("/api/v1",updateScore);
 
